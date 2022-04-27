c7f93faf9ca9d8fc4a2ef66fa8bdfc06995d1a69
IhorNovikow <gosha.noviko2012@gmail.com>
2022-04-28 00:35:38 +0300

    carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,25 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  //throw new NotImplementedError('Not implemented');
+  const HALF_LIFE_PERIOD = 5730;
+  if( +sampleActivity<0 || +sampleActivity>15 || sampleActivity=='0' || sampleActivity=='' || sampleActivity==' ' || sampleActivity==' \n\t\r' || typeof(sampleActivity)=="object"){
+    return(false)
+  }
+  if (typeof(+sampleActivity) =="number" && !isNaN(+sampleActivity) && typeof(sampleActivity) != "number"){
+    let a=Math.log(15/sampleActivity)
+    let b=0.693;
+    let c=Math.ceil(Math.log((15/sampleActivity))/(0.693/HALF_LIFE_PERIOD))
+    console.log(isFinite(c))
+    if(!isNaN(c) && isFinite(c)){
+      return(c) 
+    }else{
+      return(false)
+    }        
+  }else{
+    return(false)
+  } 
 }
 
 module.exports = {
