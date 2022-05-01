2adbeface262cfb123ad2da4e496a7f4e97a42de
IhorNovikow <gosha.noviko2012@gmail.com>
2022-05-02 02:11:16 +0300

    create tasck

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  //throw new NotImplementedError('Not implemented');
+  let a = []
+  let i = 0
+  if (s1.length == 0 || s2.length == 0) {
+    return 0
+  } else {
+    while (i < s1.length) {
+      if (s2.indexOf(s1[i]) != -1){
+        s2 = s2.replace(s1[i], '')
+        a.push(s1[i])
+      }
+      i++
+    }
+    return a.length
+  }
 }
 
 module.exports = {
--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,140 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  //throw new NotImplementedError('Not implemented');
+  let arrArr=[
+    [0,0,0],
+    [0,0,0],
+  ]
+let arrArr3=[
+    [0,0,0],
+    [0,0,0],
+    [0,0,0],
+  ]
+if(matrix.length==2){
+  if(matrix[0][0]==true){
+    arrArr[0][1]++
+    arrArr[1][0]++
+    arrArr[1][1]++
+  }
+  if(matrix[0][1]==true){
+    arrArr[0][0]++
+    arrArr[0][2]++
+    arrArr[1][1]++
+    arrArr[1][0]++
+    arrArr[1][2]++
+  }
+  if(matrix[0][2]==true){
+    arrArr[0][1]++
+    arrArr[1][2]++
+    arrArr[1][1]++
+  }
+  if(matrix[1][0]==true){
+    arrArr[0][0]++
+    arrArr[1][1]++
+    arrArr[0][1]++
+  }
+  if(matrix[1][1]==true){
+    arrArr[1][0]++
+    arrArr[0][1]++
+    arrArr[1][2]++
+    arrArr[0][0]++
+    arrArr[0][2]++
+  }
+  if(matrix[1][2]==true){
+    arrArr[0][2]++
+    arrArr[1][1]++
+    arrArr[0][1]++
+  }
+  if(matrix[0][0]==true){arrArr[0][0]=1}
+  if(matrix[0][1]==true){arrArr[0][1]=1}
+  if(matrix[0][2]==true){arrArr[0][2]=1}
+  if(matrix[1][0]==true){arrArr[1][0]=1}
+  if(matrix[1][1]==true){arrArr[1][1]=1}
+  if(matrix[1][2]==true){arrArr[1][2]=1}
+  
+  return arrArr
+  
+}else if(matrix.length==3){
+  if(matrix[0][0]==true){
+    arrArr3[0][0]=1
+    arrArr3[0][1]++
+    arrArr3[1][0]++
+    arrArr3[1][1]++
+  }
+  if(matrix[0][1]==true){
+    arrArr3[0][1]=1
+    arrArr3[0][0]++
+    arrArr3[0][2]++
+    arrArr3[1][1]++
+    arrArr3[1][0]++
+    arrArr3[1][2]++
+  }
+  if(matrix[0][2]==true){
+    arrArr3[0][2]=1
+    arrArr3[0][1]++
+    arrArr3[1][2]++
+    arrArr3[1][1]++
+  }
+  if(matrix[1][0]==true){
+    arrArr3[1][0]=1
+    arrArr3[0][0]++
+    arrArr3[1][1]++
+    arrArr3[0][1]++
+    arrArr3[2][0]++
+    arrArr3[2][1]++
+  }
+  if(matrix[1][1]==true){
+    arrArr3[1][1]=1
+    arrArr3[1][0]++
+    arrArr3[0][1]++
+    arrArr3[1][2]++
+    arrArr3[0][0]++
+    arrArr3[0][2]++
+    arrArr3[2][0]++
+    arrArr3[2][1]++
+    arrArr3[2][2]++
+  }
+  if(matrix[1][2]==true){
+    arrArr3[1][2]=1
+    arrArr3[0][2]++
+    arrArr3[1][1]++
+    arrArr3[0][1]++      
+    arrArr3[2][1]++
+    arrArr3[2][2]++
+  }
+  if(matrix[2][0]==true){      
+    arrArr3[1][0]++
+    arrArr3[1][1]++
+    arrArr3[2][1]++
+  }
+  if(matrix[2][1]==true){      
+    arrArr3[2][0]++
+    arrArr3[1][0]++
+    arrArr3[1][1]++      
+    arrArr3[1][2]++
+    arrArr3[2][2]++
+  }
+  if(matrix[2][2]==true){      
+    arrArr3[1][2]++
+    arrArr3[1][1]++
+    arrArr3[2][1]++
+  }
+  
+  if(matrix[0][0]==true){arrArr3[0][0]=1}
+  if(matrix[0][1]==true){arrArr3[0][1]=1}
+  if(matrix[0][2]==true){arrArr3[0][2]=1}
+  if(matrix[1][0]==true){arrArr3[1][0]=1}
+  if(matrix[1][1]==true){arrArr3[1][1]=1}
+  if(matrix[1][2]==true){arrArr3[1][2]=1}
+  if(matrix[2][0]==true){arrArr3[2][0]=1}
+  if(matrix[2][1]==true){arrArr3[2][1]=1}
+  if(matrix[2][2]==true){arrArr3[2][2]=1}    
+  
+  return arrArr3 
+}
+  
 }
 
 module.exports = {
